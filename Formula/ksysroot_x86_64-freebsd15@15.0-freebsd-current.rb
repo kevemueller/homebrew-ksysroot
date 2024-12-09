4eef8247b0f9fb7c9c8d919cbd46721627cb3184
Keve <kevemueller@users.noreply.github.com>
2024-12-09 22:31:43 +0100

    Add amd64/FreeBSD-15.0-CURRENT

---
+++ Formula/ksysroot_x86_64-freebsd15@15.0-freebsd-current.rb
@@ -0,0 +1,39 @@
+class KsysrootX8664Freebsd15AT150FreebsdCurrent < Formula
+  desc "Sysroot for x86_64-freebsd15@freebsd15.0-CURRENT"
+  homepage "https://github.com/kevemueller/ksysroot"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.2.tar.gz"
+  sha256 "c3b16ca49dc38e2d72d67cf71b6d153048428aed250b263f372c91289834789a"
+  license "BSD-2-Clause"
+  head "https://github.com/kevemueller/ksysroot.git", branch: "main"
+
+  depends_on "meson" => :test
+  depends_on "lld"
+  depends_on "llvm"
+  depends_on "pkgconf"
+
+  resource "base.txz" do
+    url "https://download.freebsd.org/snapshots/amd64/amd64/15.0-CURRENT/base.txz"
+    sha256 "5d9a0fcebba16c22a81310ebb9c2c5ee640f75e9f4207e6d2dc8b28dc075c8eb"
+  end
+
+  def install
+    ENV["BREW_PREFIX_LLVM"]=Formula["llvm"].prefix
+    ENV["BREW_PREFIX_LLD"]=Formula["lld"].prefix
+    ENV["BREW_PREFIX_PKGCONF"]=Formula["pkgconf"].prefix
+    ENV["BASE_TXZ"]=resource("base.txz").cached_download
+    system "./ksysroot.sh", "install", prefix, "x86_64-freebsd15@freebsd15.0-CURRENT"
+  end
+
+  test do
+    resource "testcases" do
+      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.2.tar.gz"
+      sha256 "c3b16ca49dc38e2d72d67cf71b6d153048428aed250b263f372c91289834789a"
+    end
+    resource("testcases").stage do
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+        "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
+      assert_predicate testpath/"build-c/main", :exist?
+    end
+  end
+end
