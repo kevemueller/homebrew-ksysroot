33019595c0a9f937152801821d1aa4d100c1b7b4
Keve Müller <15782588+kevemueller@users.noreply.github.com>
2024-12-10 21:05:54 +0000

    ksysroot_aarch64-freebsd14: add 0.3 bottle.

--- Formula/ksysroot_aarch64-freebsd14.rb
+++ Formula/ksysroot_aarch64-freebsd14.rb
@@ -6,6 +6,13 @@ class KsysrootAarch64Freebsd14 < Formula
   license "BSD-2-Clause"
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "252341c01a1c7859118bf689dcf91a1448efdb22bcab14828a3a1cf026f77fbd"
+    sha256 cellar: :any_skip_relocation, ventura:       "1a9de1d3436bee0acce7e21b91e592ee30875e89286f019d87703ed3c147c278"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "213b3229eccf18a30c974bc33f50a1f5f830b0d36eeb5f2f013e042fd78f0823"
+  end
+
   depends_on "meson" => :test
   depends_on "lld"
   depends_on "llvm"
