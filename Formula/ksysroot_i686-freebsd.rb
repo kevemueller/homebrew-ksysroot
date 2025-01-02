3f31f06daf53a1dc7d0d6b3d40680676afcbc860
Keve <kevemueller@users.noreply.github.com>
2025-01-02 14:48:45 +0100

    FreeBSD 14.2
    Closes #9.
    
    Signed-off-by: Keve Müller <15782588+kevemueller@users.noreply.github.com>

---
+++ Formula/ksysroot_i686-freebsd.rb
@@ -0,0 +1,83 @@
+class KsysrootI686Freebsd < Formula
+  desc "Sysroot for i686-freebsd14.2@freebsd14.2-RELEASE"
+  homepage "https://github.com/kevemueller/ksysroot"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+  license "BSD-2-Clause"
+  head "https://github.com/kevemueller/ksysroot.git", branch: "main"
+
+  depends_on "meson" => :test
+  depends_on "lld"
+  depends_on "llvm"
+  depends_on "pkgconf"
+
+  uses_from_macos "libarchive"
+
+  resource "base.txz" do
+    url "https://download.freebsd.org/releases/i386/i386/14.2-RELEASE/base.txz"
+    version "14.2-RELEASE"
+    sha256 "2b8043a6d0b8c53c16e205db540744170eccd91a7475d3c3ac43f62041317c0f"
+  end
+
+  def install
+    cachedir=ENV.fetch("HOMEBREW_CACHE")
+    ENV["CACHE_DIR"]=cachedir
+    ENV["LLVM_DIR"]=Formula["llvm"].bin
+    ENV["LLD_DIR"]=Formula["lld"].bin
+    ENV["PKG_CONFIG"]="#{Formula["pkgconf"].bin}/pkg-config"
+    bom = <<~EOS
+      # KSYSROOT_TRIPLE=i686-freebsd14.2 KSYSROOT_FULL_TRIPLE=i686-freebsd14.2
+      # KSYSROOT_OSFLAVOUR=freebsd KSYSROOT_OSRELEASE=14.2-RELEASE
+      # KSYSROOT_LINKER=ld.lld
+      # MESON_SYSTEM=freebsd MESON_CPUFAMILY=x86 MESON_CPU=x86 MESON_ENDIAN=little
+      # FREEBSD_VERSION=14.2-RELEASE FREEBSD_MACHINE=i386 FREEBSD_MACHINE_ARCH=i386
+    EOS
+    bom << resources.map { |r|
+      "#{r.name} #{r.version} #{r.url} #{r.cached_download.to_s.delete_prefix(cachedir)}"
+    }.join("\n")
+    bom << "\n"
+    ohai "bom=#{bom}"
+    File.write("bom.in", bom)
+    system "./ksysroot.sh", "frombom", prefix, "bom.in"
+  end
+
+  test do
+    resource "testcases" do
+      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
+      sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+    end
+    resource("testcases").stage do
+      ENV.delete("CC")
+      ENV.delete("CXX")
+      ENV.delete("CXX")
+      ENV.delete("OBJC")
+      ENV.delete("OBJCXX")
+      ENV.delete("CFLAGS")
+      ENV.delete("CPPFLAGS")
+      ENV.delete("CXXFLAGS")
+      ENV.delete("LDFLAGS")
+      ENV.delete("LD_RUN_PATH")
+      ENV.delete("LIBRARY_PATH")
+      ENV.delete("OBJCFLAGS")
+      ENV.delete("OBJCXXFLAGS")
+      ENV.delete("CPATH")
+      ENV.delete("PKG_CONFIG_LIBDIR")
+      system "set"
+      # cross build a C library + program
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
+      assert_predicate testpath/"build-c/main", :exist?
+
+      # cross build a C++ library + program
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
+      assert_predicate testpath/"build-cxx/main", :exist?
+
+      # check pkg-config personality is proper
+      assert_equal "-lcrypto", shell_output("#{bin}/i686-freebsd14.2-pkg-config --libs libcrypto").strip
+      assert_equal "", shell_output("#{bin}/i686-freebsd14.2-pkg-config --cflags libcrypto").strip
+    end
+  end
+end
