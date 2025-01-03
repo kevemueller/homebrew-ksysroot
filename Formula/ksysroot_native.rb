5a42945aa1e47d9cf2ffea9b378c3ceb44f7eff9
Keve <kevemueller@users.noreply.github.com>
2025-01-03 15:18:15 +0100

    native
    Closes #16.
    
    Signed-off-by: Keve Müller <15782588+kevemueller@users.noreply.github.com>

--- Formula/ksysroot_native.rb
+++ Formula/ksysroot_native.rb
@@ -1,8 +1,8 @@
 class KsysrootNative < Formula
-  desc "Sysroot for native builds"
+  desc "Sysroot for native@macos15.2"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.2.tar.gz"
-  sha256 "c3b16ca49dc38e2d72d67cf71b6d153048428aed250b263f372c91289834789a"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
   license "BSD-2-Clause"
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
 
@@ -12,21 +12,47 @@ class KsysrootNative < Formula
   depends_on "pkgconf"
 
   def install
-    ENV["BREW_PREFIX_LLVM"]=Formula["llvm"].prefix
-    ENV["BREW_PREFIX_LLD"]=Formula["lld"].prefix
-    ENV["BREW_PREFIX_PKGCONF"]=Formula["pkgconf"].prefix
-    system "./ksysroot.sh", "install", prefix, "native"
+    cachedir=ENV.fetch("HOMEBREW_CACHE")
+    ENV["CACHE_DIR"]=cachedir
+    ENV["LLVM_DIR"]=Formula["llvm"].bin
+    ENV["LLD_DIR"]=Formula["lld"].bin
+    ENV["PKG_CONFIG"]="#{Formula["pkgconf"].bin}/pkg-config"
+    system "./ksysroot.sh", "install", "native", prefix
   end
-
   test do
     resource "testcases" do
-      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.2.tar.gz"
-      sha256 "c3b16ca49dc38e2d72d67cf71b6d153048428aed250b263f372c91289834789a"
+      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
+      sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
     end
     resource("testcases").stage do
-      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt", testpath/"build-c", "test-c"
+      ENV.delete("CC")
+      ENV.delete("CXX")
+      ENV.delete("CXX")
+      ENV.delete("OBJC")
+      ENV.delete("OBJCXX")
+      ENV.delete("CFLAGS")
+      ENV.delete("CPPFLAGS")
+      ENV.delete("CXXFLAGS")
+      ENV.delete("LDFLAGS")
+      ENV.delete("LD_RUN_PATH")
+      ENV.delete("LIBRARY_PATH")
+      ENV.delete("OBJCFLAGS")
+      ENV.delete("OBJCXXFLAGS")
+      ENV.delete("CPATH")
+      ENV.delete("PKG_CONFIG_LIBDIR")
+      system "set"
+      # build a C library + program
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             testpath/"build-c", "test-c"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
       assert_predicate testpath/"build-c/main", :exist?
+
+      # build a C++ library + program
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             testpath/"build-cxx", "test-cxx"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
+      assert_predicate testpath/"build-cxx/main", :exist?
+      system "#{bin}/native-pkg-config", "--list-all"
     end
   end
 end
