c7c53d6fd74078da7b1bbdab95dd5c4248897962
Keve <kevemueller@users.noreply.github.com>
2025-01-06 10:14:11 +0100

    update ksysroot_mips64el-linux6.12-gnuabi64@13-debian

--- Formula/ksysroot_mips64el-linux6.12-gnuabi64@13-debian.rb
+++ Formula/ksysroot_mips64el-linux6.12-gnuabi64@13-debian.rb
@@ -4,17 +4,16 @@ class KsysrootMips64elLinux612Gnuabi64AT13Debian < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
   sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
 
   keg_only :versioned_formula
-
   depends_on "meson" => :test
   depends_on "lld"
   depends_on "llvm"
   depends_on "pkgconf"
 
   uses_from_macos "libarchive"
-
   on_sonoma :or_older do
     # for sha256sum
     depends_on "coreutils"
@@ -114,6 +113,7 @@ class KsysrootMips64elLinux612Gnuabi64AT13Debian < Formula
       # KSYSROOT_TRIPLE=mips64el-linux-gnuabi64 KSYSROOT_FULL_TRIPLE=mips64el-linux6.12-gnuabi64
       # KSYSROOT_OSFLAVOUR=debian KSYSROOT_OSRELEASE=13
       # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=GPL-2.0-or-later
       # MESON_SYSTEM=linux MESON_CPUFAMILY=mips64 MESON_CPU=mips64 MESON_ENDIAN=little
       # DEBIAN_VERSION=13 DEBIAN_NAME=trixie DEBIAN_GCC=14
       # DEBIAN_ARCH=mips64el LINUX_VERSION=6.12
@@ -126,5 +126,43 @@ class KsysrootMips64elLinux612Gnuabi64AT13Debian < Formula
     ohai "bom=#{bom}"
     File.write("bom.in", bom)
     system "./ksysroot.sh", "frombom", prefix, "bom.in"
+  end
+  test do
+    resource "testcases" do
+      url KsysrootMips64elLinux612Gnuabi64AT13Debian.stable.url
+      sha256 KsysrootMips64elLinux612Gnuabi64AT13Debian.stable.checksum.hexdigest
+    end
+    resource("testcases").stage do
+      ENV.delete("CC")
+      ENV.delete("CXX")
+      ENV.delete("CXX")
+      ENV.delete("OBJC")
+      ENV.delete("OBJCXX")
+      ENV.delete("CFLAGS")
+      ENV.delete("CPPFLAGS")
+      ENV.delete("CXXFLAGS")
+      ENV.delete("LDFLAGS")
+      ENV.delete("LD_RUN_PATH")
+      ENV.delete("LIBRARY_PATH")
+      ENV.delete("OBJCFLAGS")
+      ENV.delete("OBJCXXFLAGS")
+      ENV.delete("CPATH")
+      ENV.delete("PKG_CONFIG_LIBDIR")
+      system "set"
+      # build a C library + program with meson
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
+      assert_predicate testpath/"build-c/main", :exist?
+
+      # build a C++ library + program with meson
+      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
+             "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
+      assert_predicate testpath/"build-cxx/main", :exist?
+      # check pkg-config personality is properly set-up
+      assert_equal "-lcrypt", shell_output("#{bin}/mips64el-linux-gnuabi64-pkg-config --libs libcrypt").strip
+      assert_equal "", shell_output("#{bin}/mips64el-linux-gnuabi64-pkg-config --cflags libcrypt").strip
+    end
   end
 end
