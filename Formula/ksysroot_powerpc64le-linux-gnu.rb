90473147058b0018241e1f45ef2e64356360be89
Keve <kevemueller@users.noreply.github.com>
2025-01-06 10:14:34 +0100

    update ksysroot_powerpc64le-linux-gnu
    Closes #66.
    
    Signed-off-by: Keve Müller <15782588+kevemueller@users.noreply.github.com>

--- Formula/ksysroot_powerpc64le-linux-gnu.rb
+++ Formula/ksysroot_powerpc64le-linux-gnu.rb
@@ -4,14 +4,8 @@ class KsysrootPowerpc64leLinuxGnu < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
   sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
   license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d89761fdfdef8992e90740ffdb694ca5352e4e1bffad148b111f12d314a299e8"
-    sha256 cellar: :any_skip_relocation, ventura:       "b6607909d1e08560d6b9222408a9b37aa8b57c0ca1cb4d99e0eceacd3a23527b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c24e170535eab7be47b0923f50604a846bddf2dd9fab1ec69f26ce4fe7b0c423"
-  end
 
   depends_on "meson" => :test
   depends_on "lld"
@@ -19,7 +13,6 @@ class KsysrootPowerpc64leLinuxGnu < Formula
   depends_on "pkgconf"
 
   uses_from_macos "libarchive"
-
   on_sonoma :or_older do
     # for sha256sum
     depends_on "coreutils"
@@ -227,6 +220,7 @@ class KsysrootPowerpc64leLinuxGnu < Formula
       # KSYSROOT_TRIPLE=powerpc64le-linux-gnu KSYSROOT_FULL_TRIPLE=powerpc64le-linux6.1-gnu
       # KSYSROOT_OSFLAVOUR=debian KSYSROOT_OSRELEASE=12
       # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=GPL-2.0-or-later
       # MESON_SYSTEM=linux MESON_CPUFAMILY=ppc64 MESON_CPU=ppc64 MESON_ENDIAN=little
       # DEBIAN_VERSION=12 DEBIAN_NAME=bookworm DEBIAN_GCC=12
       # DEBIAN_ARCH=ppc64el LINUX_VERSION=6.1
@@ -242,8 +236,8 @@ class KsysrootPowerpc64leLinuxGnu < Formula
   end
   test do
     resource "testcases" do
-      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
-      sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
+      url KsysrootPowerpc64leLinuxGnu.stable.url
+      sha256 KsysrootPowerpc64leLinuxGnu.stable.checksum.hexdigest
     end
     resource("testcases").stage do
       ENV.delete("CC")
@@ -262,18 +256,18 @@ class KsysrootPowerpc64leLinuxGnu < Formula
       ENV.delete("CPATH")
       ENV.delete("PKG_CONFIG_LIBDIR")
       system "set"
-      # build a C library + program
+      # build a C library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
       assert_predicate testpath/"build-c/main", :exist?
 
-      # build a C++ library + program
+      # build a C++ library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
       assert_predicate testpath/"build-cxx/main", :exist?
-      # check pkg-config personality is proper
+      # check pkg-config personality is properly set-up
       assert_equal "-lcrypt", shell_output("#{bin}/powerpc64le-linux-gnu-pkg-config --libs libcrypt").strip
       assert_equal "", shell_output("#{bin}/powerpc64le-linux-gnu-pkg-config --cflags libcrypt").strip
     end
