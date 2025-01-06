01fe27d671cbf3e4a8e8f931fa0c2858960fea6f
Keve <kevemueller@users.noreply.github.com>
2025-01-06 10:19:18 +0100

    update ksysroot_powerpc-freebsd
    Closes #105.
    
    Signed-off-by: Keve Müller <15782588+kevemueller@users.noreply.github.com>

--- Formula/ksysroot_powerpc-freebsd.rb
+++ Formula/ksysroot_powerpc-freebsd.rb
@@ -1,17 +1,10 @@
 class KsysrootPowerpcFreebsd < Formula
   desc "Sysroot for powerpc-freebsd14.2@freebsd14.2-RELEASE"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
   license "BSD-2-Clause"
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb42e96cfff18e1669326ff5d68b68d1f5aebf8ad0939edbdfa50aec6195d8b8"
-    sha256 cellar: :any_skip_relocation, ventura:       "af145e2cca82417fa50694ba027f6e6317d2b01320ccbbd90c192bdb710c9fc3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef42ccc81afd1ea755ac2d58b732c0b688ab8437e1e32148c6f6f8638c604e8c"
-  end
 
   depends_on "meson" => :test
   depends_on "lld"
@@ -19,10 +12,14 @@ class KsysrootPowerpcFreebsd < Formula
   depends_on "pkgconf"
 
   uses_from_macos "libarchive"
+  on_sonoma :or_older do
+    # for sha256sum
+    depends_on "coreutils"
+  end
 
   resource "base.txz" do
     url "https://download.freebsd.org/releases/powerpc/powerpc/14.2-RELEASE/base.txz"
-    version "14.2-RELEASE"
+    version "14.2-RELEASE-ksr"
     sha256 "16a791239a714e5a055fc5527e50fdbb5b9332349278221504ba8671387915d9"
   end
 
@@ -36,22 +33,23 @@ class KsysrootPowerpcFreebsd < Formula
       # KSYSROOT_TRIPLE=powerpc-freebsd14.2 KSYSROOT_FULL_TRIPLE=powerpc-freebsd14.2
       # KSYSROOT_OSFLAVOUR=freebsd KSYSROOT_OSRELEASE=14.2-RELEASE
       # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=BSD-2-Clause
       # MESON_SYSTEM=freebsd MESON_CPUFAMILY=ppc MESON_CPU=ppc MESON_ENDIAN=little
       # FREEBSD_VERSION=14.2-RELEASE FREEBSD_MACHINE=powerpc FREEBSD_MACHINE_ARCH=powerpc
     EOS
     bom << resources.map { |r|
-      "#{r.name} #{r.version} #{r.url} #{r.cached_download.to_s.delete_prefix(cachedir)}"
+      "#{r.name} #{r.version.to_s.delete_suffix("-ksr")} #{r.url} " \
+        "#{r.cached_download.relative_path_from(cachedir)} #{r.checksum}"
     }.join("\n")
     bom << "\n"
     ohai "bom=#{bom}"
     File.write("bom.in", bom)
     system "./ksysroot.sh", "frombom", prefix, "bom.in"
   end
-
   test do
     resource "testcases" do
-      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-      sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+      url KsysrootPowerpcFreebsd.stable.url
+      sha256 KsysrootPowerpcFreebsd.stable.checksum.hexdigest
     end
     resource("testcases").stage do
       ENV.delete("CC")
@@ -70,19 +68,18 @@ class KsysrootPowerpcFreebsd < Formula
       ENV.delete("CPATH")
       ENV.delete("PKG_CONFIG_LIBDIR")
       system "set"
-      # cross build a C library + program
+      # build a C library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
       assert_predicate testpath/"build-c/main", :exist?
 
-      # cross build a C++ library + program
+      # build a C++ library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
       assert_predicate testpath/"build-cxx/main", :exist?
-
-      # check pkg-config personality is proper
+      # check pkg-config personality is properly set-up
       assert_equal "-lcrypto", shell_output("#{bin}/powerpc-freebsd14.2-pkg-config --libs libcrypto").strip
       assert_equal "", shell_output("#{bin}/powerpc-freebsd14.2-pkg-config --cflags libcrypto").strip
     end
