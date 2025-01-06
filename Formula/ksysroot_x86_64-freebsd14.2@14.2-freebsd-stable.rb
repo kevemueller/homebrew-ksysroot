2ff78453020734ac74611712071dad50c9648fa3
Keve <kevemueller@users.noreply.github.com>
2025-01-06 10:17:21 +0100

    update ksysroot_x86_64-freebsd14.2@14.2-freebsd-stable

--- Formula/ksysroot_x86_64-freebsd14.2@14.2-freebsd-stable.rb
+++ Formula/ksysroot_x86_64-freebsd14.2@14.2-freebsd-stable.rb
@@ -1,23 +1,26 @@
 class KsysrootX8664Freebsd142AT142FreebsdStable < Formula
   desc "Sysroot for x86_64-freebsd14.2@freebsd14.2-STABLE"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
   license "BSD-2-Clause"
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
 
   keg_only :versioned_formula
-
   depends_on "meson" => :test
   depends_on "lld"
   depends_on "llvm"
   depends_on "pkgconf"
 
   uses_from_macos "libarchive"
+  on_sonoma :or_older do
+    # for sha256sum
+    depends_on "coreutils"
+  end
 
   resource "base.txz" do
     url "https://download.freebsd.org/snapshots/amd64/amd64/14.2-STABLE/base.txz"
-    version "14.2-STABLE"
+    version "14.2-STABLE-ksr"
     sha256 "df530f3bcc4dea3351a0c8109bef0d80b4fddae6e5c272270b38addca0ed4cee"
   end
 
@@ -31,22 +34,23 @@ class KsysrootX8664Freebsd142AT142FreebsdStable < Formula
       # KSYSROOT_TRIPLE=x86_64-freebsd14.2 KSYSROOT_FULL_TRIPLE=x86_64-freebsd14.2
       # KSYSROOT_OSFLAVOUR=freebsd KSYSROOT_OSRELEASE=14.2-STABLE
       # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=BSD-2-Clause
       # MESON_SYSTEM=freebsd MESON_CPUFAMILY=x86_64 MESON_CPU=x86_64 MESON_ENDIAN=little
       # FREEBSD_VERSION=14.2-STABLE FREEBSD_MACHINE=amd64 FREEBSD_MACHINE_ARCH=amd64
     EOS
     bom << resources.map { |r|
-      "#{r.name} #{r.version} #{r.url} #{r.cached_download.to_s.delete_prefix(cachedir)}"
+      "#{r.name} #{r.version.to_s.delete_suffix("-ksr")} #{r.url} " \
+        "#{r.cached_download.relative_path_from(cachedir)} #{r.checksum}"
     }.join("\n")
     bom << "\n"
     ohai "bom=#{bom}"
     File.write("bom.in", bom)
     system "./ksysroot.sh", "frombom", prefix, "bom.in"
   end
-
   test do
     resource "testcases" do
-      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-      sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+      url KsysrootX8664Freebsd142AT142FreebsdStable.stable.url
+      sha256 KsysrootX8664Freebsd142AT142FreebsdStable.stable.checksum.hexdigest
     end
     resource("testcases").stage do
       ENV.delete("CC")
@@ -65,19 +69,18 @@ class KsysrootX8664Freebsd142AT142FreebsdStable < Formula
       ENV.delete("CPATH")
       ENV.delete("PKG_CONFIG_LIBDIR")
       system "set"
-      # cross build a C library + program
+      # build a C library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
       assert_predicate testpath/"build-c/main", :exist?
 
-      # cross build a C++ library + program
+      # build a C++ library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
       assert_predicate testpath/"build-cxx/main", :exist?
-
-      # check pkg-config personality is proper
+      # check pkg-config personality is properly set-up
       assert_equal "-lcrypto", shell_output("#{bin}/x86_64-freebsd14.2-pkg-config --libs libcrypto").strip
       assert_equal "", shell_output("#{bin}/x86_64-freebsd14.2-pkg-config --cflags libcrypto").strip
     end
