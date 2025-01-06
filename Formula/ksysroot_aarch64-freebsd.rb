91895278a332e59ce229fedefaa4b5dc844b15b3
Keve <kevemueller@users.noreply.github.com>
2025-01-06 10:19:44 +0100

    update ksysroot_aarch64-freebsd
    Closes #112.
    
    Signed-off-by: Keve Müller <15782588+kevemueller@users.noreply.github.com>

--- Formula/ksysroot_aarch64-freebsd.rb
+++ Formula/ksysroot_aarch64-freebsd.rb
@@ -1,17 +1,10 @@
 class KsysrootAarch64Freebsd < Formula
   desc "Sysroot for aarch64-freebsd14.2@freebsd14.2-RELEASE"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
+  sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
   license "BSD-2-Clause"
   head "https://github.com/kevemueller/ksysroot.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "473ebe94f98588de244b71e7bd0a0df34c76cc114b2d21290b81b6f1ea0576d5"
-    sha256 cellar: :any_skip_relocation, ventura:       "353b370d47c94f9628435ef3830b0d1e75375129785d5f1aa4a1daad92e626bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5aa84aad41c5dd0b573ac94e2d350e179fd69e61613960ceeda4dd71ad75d576"
-  end
 
   depends_on "meson" => :test
   depends_on "lld"
@@ -19,10 +12,14 @@ class KsysrootAarch64Freebsd < Formula
   depends_on "pkgconf"
 
   uses_from_macos "libarchive"
+  on_sonoma :or_older do
+    # for sha256sum
+    depends_on "coreutils"
+  end
 
   resource "base.txz" do
     url "https://download.freebsd.org/releases/arm64/aarch64/14.2-RELEASE/base.txz"
-    version "14.2-RELEASE"
+    version "14.2-RELEASE-ksr"
     sha256 "a6c3bb2310c25b7dc4130e57173cd1c5d8650bc4b0c73f254b3a712e5482d33d"
   end
 
@@ -36,22 +33,23 @@ class KsysrootAarch64Freebsd < Formula
       # KSYSROOT_TRIPLE=aarch64-freebsd14.2 KSYSROOT_FULL_TRIPLE=aarch64-freebsd14.2
       # KSYSROOT_OSFLAVOUR=freebsd KSYSROOT_OSRELEASE=14.2-RELEASE
       # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=BSD-2-Clause
       # MESON_SYSTEM=freebsd MESON_CPUFAMILY=aarch64 MESON_CPU=aarch64 MESON_ENDIAN=little
       # FREEBSD_VERSION=14.2-RELEASE FREEBSD_MACHINE=arm64 FREEBSD_MACHINE_ARCH=aarch64
     EOS
     bom << resources.map { |r|
-      "#{r.name} #{r.version} #{r.url} #{r.cached_download.to_s.delete_prefix(cachedir)}"
+      "#{r.name} #{r.version.to_s.delete_suffix("-ksr")} #{r.url} " \
+        "#{r.cached_download.relative_path_from(cachedir)} #{r.checksum}"
     }.join("\n")
     bom << "\n"
     ohai "bom=#{bom}"
     File.write("bom.in", bom)
     system "./ksysroot.sh", "frombom", prefix, "bom.in"
   end
-
   test do
     resource "testcases" do
-      url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.1.tar.gz"
-      sha256 "65da7faaa17289fb4376f611df6f06f91abceaa1798562873094a4c426aacb2f"
+      url KsysrootAarch64Freebsd.stable.url
+      sha256 KsysrootAarch64Freebsd.stable.checksum.hexdigest
     end
     resource("testcases").stage do
       ENV.delete("CC")
@@ -70,19 +68,18 @@ class KsysrootAarch64Freebsd < Formula
       ENV.delete("CPATH")
       ENV.delete("PKG_CONFIG_LIBDIR")
       system "set"
-      # cross build a C library + program
+      # build a C library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
       assert_predicate testpath/"build-c/main", :exist?
 
-      # cross build a C++ library + program
+      # build a C++ library + program with meson
       system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
              "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
       assert_predicate testpath/"build-cxx/main", :exist?
-
-      # check pkg-config personality is proper
+      # check pkg-config personality is properly set-up
       assert_equal "-lcrypto", shell_output("#{bin}/aarch64-freebsd14.2-pkg-config --libs libcrypto").strip
       assert_equal "", shell_output("#{bin}/aarch64-freebsd14.2-pkg-config --cflags libcrypto").strip
     end
