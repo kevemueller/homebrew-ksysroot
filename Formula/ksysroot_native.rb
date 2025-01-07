307b5b562da7b8d2b395798b2261bb8fb84860e2
Keve <kevemueller@users.noreply.github.com>
2025-01-07 15:28:30 +0100

    update ksysroot_native

--- Formula/ksysroot_native.rb
+++ Formula/ksysroot_native.rb
@@ -4,14 +4,8 @@ class KsysrootNative < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.7.1.tar.gz"
   sha256 "023d15752c0908cabd9630b5356ec7d49f5890a5b5411157c4114c3b866cec7c"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bf56aaea817eaa9d08e3b7f7d9ae83e93abd7289d7aa2393101665f24bc03066"
-    sha256 cellar: :any_skip_relocation, ventura:       "bcb70bf39a368c4732931a082bce1a870345521cbf54128be4b96aa9fd5b9f5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "558a63eb154a8bdc4b27130d6b4e79ed0f69d68c357a75a62bf5b95d2f07d193"
-  end
 
   depends_on "meson" => :test
   depends_on "lld"
@@ -67,7 +61,7 @@ class KsysrootNative < Formula
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
       assert_predicate testpath/"build-cxx/main", :exist?
       # check that pkg-config runs
-      system "#{bin}/native-pkg-config", "--list-all"
+      system bin/"native-pkg-config", "--list-all"
     end
   end
 end
