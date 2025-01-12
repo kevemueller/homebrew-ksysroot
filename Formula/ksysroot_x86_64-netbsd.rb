f9efe36daf60dfd9a1a15200868438016eef1fa7
Keve <kevemueller@users.noreply.github.com>
2025-01-12 21:05:15 +0100

    new

---
+++ Formula/ksysroot_x86_64-netbsd.rb
@@ -0,0 +1,64 @@
+class KsysrootX8664Netbsd < Formula
+  desc "Sysroot for x86_64-netbsd10.1@NetBSD10.1"
+  homepage "https://github.com/kevemueller/ksysroot"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.tar.gz"
+  sha256 "7be9578afc0ec7d47874ee8bc6d3457f1b703241a1ff47dbd3906f88b5200f6a"
+  license "BSD-4-Clause-UC"
+  head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
+
+  depends_on "meson" => :test
+  depends_on "ksysroot_native"
+  depends_on "lld"
+  depends_on "llvm"
+  depends_on "pkgconf"
+
+  uses_from_macos "libarchive"
+  on_sonoma :or_older do
+    # for sha256sum
+    depends_on "coreutils"
+  end
+  on_linux do
+    disable! date: "2024-01-01", because: "Unwanted system libraries"
+  end
+
+  resource "base.tar.xz" do
+    url "https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/amd64/binary/sets/base.tar.xz"
+    version "10.1-ksr"
+    sha256 "ec59d1198dad7de81771bfb93f50425241bc51331b4c50ba7a163ca4d6acd427"
+  end
+
+  resource "comp.tar.xz" do
+    url "https://cdn.netbsd.org/pub/NetBSD/NetBSD-10.1/amd64/binary/sets/comp.tar.xz"
+    version "10.1-ksr"
+    sha256 "02e51e63e05b54f9d30d4d566c55e96e1b8b36fb8e870b1cc3ed9494d93d11f3"
+  end
+
+  def install
+    cachedir=ENV.fetch("HOMEBREW_CACHE")
+    ENV["CACHE_DIR"]=cachedir
+    ENV["LLVM_DIR"]=Formula["llvm"].bin
+    ENV["LLD_DIR"]=Formula["lld"].bin
+    ENV["PKG_CONFIG"]="#{Formula["pkgconf"].bin}/pkg-config"
+    bom = <<~EOS
+      # KSYSROOT_TRIPLE=x86_64-netbsd10.1 KSYSROOT_FULL_TRIPLE=x86_64-netbsd10.1
+      # KSYSROOT_OSFLAVOUR=NetBSD KSYSROOT_OSRELEASE=10.1
+      # KSYSROOT_LINKER=ld.lld
+      # KSYSROOT_LICENSE=BSD-4-Clause-UC
+      # MESON_SYSTEM=netbsd MESON_CPUFAMILY=x86_64 MESON_CPU=x86_64 MESON_ENDIAN=little
+      # NETBSD_VERSION=10.1 NETBSD_KERNEL=10.1 NETBSD_PORT=amd64
+    EOS
+    bom << resources.map { |r|
+      "#{r.name} #{r.version.to_s.delete_suffix("-ksr")} #{r.url} " \
+        "#{r.cached_download.relative_path_from(cachedir)} #{r.checksum}"
+    }.join("\n")
+    bom << "\n"
+    File.write("bom.in", bom)
+    link_triple="x86_64-netbsd"
+    system "./ksysroot.sh", "frombom", prefix, "bom.in", link_triple
+    rm prefix/"native.txt"
+    meson_cross = share/"meson/cross"
+    mkdir meson_cross
+    meson_cross.install prefix/"cross.txt" => "x86_64-netbsd10.1"
+    meson_cross.install_symlink meson_cross/"x86_64-netbsd10.1" => link_triple unless link_triple.empty?
+  end
+end
