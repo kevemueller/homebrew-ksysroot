0f23a563e9a113284b705cc966667f6459d6781d
Keve <kevemueller@users.noreply.github.com>
2025-01-12 22:13:43 +0100

    update ksysroot_i686-freebsd14.2@14.2-freebsd-stable

--- Formula/ksysroot_i686-freebsd14.2@14.2-freebsd-stable.rb
+++ Formula/ksysroot_i686-freebsd14.2@14.2-freebsd-stable.rb
@@ -1,13 +1,13 @@
 class KsysrootI686Freebsd142AT142FreebsdStable < Formula
-  desc "Sysroot for i686-freebsd14.2@freebsd14.2-STABLE"
+  desc "Sysroot for i686-freebsd14.2@FreeBSD14.2-STABLE"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.6.4.tar.gz"
-  sha256 "b8d0954e9d71aa5b10f2d41b4279287cb235d7dbcfc0bc431ffaa98034c4d884"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.tar.gz"
+  sha256 "7be9578afc0ec7d47874ee8bc6d3457f1b703241a1ff47dbd3906f88b5200f6a"
   license "BSD-2-Clause"
-  head "https://github.com/kevemueller/ksysroot.git", branch: "main"
+  head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
 
-  keg_only :versioned_formula
   depends_on "meson" => :test
+  depends_on "ksysroot_native"
   depends_on "lld"
   depends_on "llvm"
   depends_on "pkgconf"
@@ -21,7 +21,7 @@ class KsysrootI686Freebsd142AT142FreebsdStable < Formula
   resource "base.txz" do
     url "https://download.freebsd.org/snapshots/i386/i386/14.2-STABLE/base.txz"
     version "14.2-STABLE-ksr"
-    sha256 "dd05b669acc9702ad244abdac0e83ae8e84677653005029bb2f01871e64f6d92"
+    sha256 "2a138454035b338a2072b24360e36bfd797d865b333df2599471ea6b1a6869ad"
   end
 
   def install
@@ -32,20 +32,25 @@ class KsysrootI686Freebsd142AT142FreebsdStable < Formula
     ENV["PKG_CONFIG"]="#{Formula["pkgconf"].bin}/pkg-config"
     bom = <<~EOS
       # KSYSROOT_TRIPLE=i686-freebsd14.2 KSYSROOT_FULL_TRIPLE=i686-freebsd14.2
-      # KSYSROOT_OSFLAVOUR=freebsd KSYSROOT_OSRELEASE=14.2-STABLE
+      # KSYSROOT_OSFLAVOUR=FreeBSD KSYSROOT_OSRELEASE=14.2-STABLE
       # KSYSROOT_LINKER=ld.lld
       # KSYSROOT_LICENSE=BSD-2-Clause
       # MESON_SYSTEM=freebsd MESON_CPUFAMILY=x86 MESON_CPU=x86 MESON_ENDIAN=little
-      # FREEBSD_VERSION=14.2-STABLE FREEBSD_MACHINE=i386 FREEBSD_MACHINE_ARCH=i386
+      # FREEBSD_VERSION=14.2-STABLE FREEBSD_KERNEL=14.2 FREEBSD_MACHINE=i386 FREEBSD_MACHINE_ARCH=i386
     EOS
     bom << resources.map { |r|
       "#{r.name} #{r.version.to_s.delete_suffix("-ksr")} #{r.url} " \
         "#{r.cached_download.relative_path_from(cachedir)} #{r.checksum}"
     }.join("\n")
     bom << "\n"
-    ohai "bom=#{bom}"
     File.write("bom.in", bom)
-    system "./ksysroot.sh", "frombom", prefix, "bom.in"
+    link_triple=""
+    system "./ksysroot.sh", "frombom", prefix, "bom.in", link_triple
+    rm prefix/"native.txt"
+    meson_cross = share/"meson/cross"
+    mkdir meson_cross
+    meson_cross.install prefix/"cross.txt" => "i686-freebsd14.2"
+    meson_cross.install_symlink meson_cross/"i686-freebsd14.2" => link_triple unless link_triple.empty?
   end
   test do
     resource "testcases" do
@@ -69,17 +74,13 @@ class KsysrootI686Freebsd142AT142FreebsdStable < Formula
       ENV.delete("CPATH")
       ENV.delete("PKG_CONFIG_LIBDIR")
       system "set"
-      # build a C library + program with meson
-      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
-             "--cross-file=#{prefix}/cross.txt", testpath/"build-c", "test-c"
-      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-c"
-      assert_predicate testpath/"build-c/main", :exist?
-
-      # build a C++ library + program with meson
-      system Formula["meson"].bin/"meson", "setup", "--native-file=#{prefix}/native.txt",
-             "--cross-file=#{prefix}/cross.txt", testpath/"build-cxx", "test-cxx"
-      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build-cxx"
-      assert_predicate testpath/"build-cxx/main", :exist?
+      # build a C and C++ library + program with meson
+      system Formula["meson"].bin/"meson", "setup", "--native-file=ksysroot",
+             "--cross-file=i686-freebsd14.2", testpath/"build"
+      system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build"
+      # test for the executables
+      assert_predicate testpath/"build/test-c/main", :exist?
+      assert_predicate testpath/"build/test-cxx/main", :exist?
       # check pkg-config personality is properly set-up
       assert_equal "-lcrypto", shell_output("#{bin}/i686-freebsd14.2-pkg-config --libs libcrypto").strip
       assert_equal "", shell_output("#{bin}/i686-freebsd14.2-pkg-config --cflags libcrypto").strip
