9365a151f77dbb1b2b91ba1a920a95f4ce19ed21
Keve <kevemueller@users.noreply.github.com>
2025-01-13 13:37:30 +0100

    update ksysroot_i386-linux-gnu

--- Formula/ksysroot_i386-linux-gnu.rb
+++ Formula/ksysroot_i386-linux-gnu.rb
@@ -1,17 +1,10 @@
 class KsysrootI386LinuxGnu < Formula
   desc "Sysroot for i386-linux-gnu@Debian12"
   homepage "https://github.com/kevemueller/ksysroot"
-  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.tar.gz"
-  sha256 "7be9578afc0ec7d47874ee8bc6d3457f1b703241a1ff47dbd3906f88b5200f6a"
+  url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "1091e2e6314c063e00234cb7694b5d03900c292f5025ebcc362770079a612d2e"
   license "GPL-2.0-or-later"
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e8583bd0681ef5c20a5186bddef03f19ed8602e04bfb2da52d8e1734c98b2dd1"
-    sha256 cellar: :any_skip_relocation, ventura:       "33ad65105fb6218cbbb5c621fc56cc8b70663dd1ad1626d2580a574c7251b48c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2c85e8961d25e26acd336fbb94cdcc6ac3956338a310b202dcdd1b272ee453c"
-  end
 
   depends_on "meson" => :test
   depends_on "ksysroot_native"
