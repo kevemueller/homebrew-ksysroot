3351ac7cac9c8e8a2e24b6a187be81510ab4fa50
Keve Müller <15782588+kevemueller@users.noreply.github.com>
2025-03-03 19:42:25 +0000

    kyua: add 0.14 bottle.

--- Formula/kyua.rb
+++ Formula/kyua.rb
@@ -6,6 +6,14 @@ class Kyua < Formula
   license "BSD-3-Clause"
   head "https://github.com/freebsd/kyua.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
+    rebuild 1
+    sha256 arm64_sequoia: "b5bfc1bf190186b9130cd9bae15e67d5afc1dd39d0fca5379ee8a212d0ffe45a"
+    sha256 ventura:       "f52092e829ae14b2971a3c32d8db469c1f807a140872346bf79e1b5c6d7f6f44"
+    sha256 x86_64_linux:  "32e213f817bf7431ee88ca51d26bea866e24a92e72f253180f72981990520013"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
