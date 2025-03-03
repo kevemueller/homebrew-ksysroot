0821a5ee655da691de64a9b781776c29877d4961
Keve <kevemueller@users.noreply.github.com>
2025-03-03 19:51:54 +0100

    update ksysroot_loongarch64-linux-musl

--- Formula/ksysroot_loongarch64-linux-musl.rb
+++ Formula/ksysroot_loongarch64-linux-musl.rb
@@ -4,14 +4,8 @@ class KsysrootLoongarch64LinuxMusl < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.1.tar.gz"
   sha256 "1091e2e6314c063e00234cb7694b5d03900c292f5025ebcc362770079a612d2e"
   license "MIT"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b96c17d2a2fb7961052879c62938243701525c3a040c20db085f3e23093fb43e"
-    sha256 cellar: :any_skip_relocation, ventura:       "c4cca09b8ba3ad170540fe43d81a85f905dc9dcc859556d2391ad85b59fa69a4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9b071fa5364473504494be4f5450f2a1c567553f9c55f29c520e5020991de41"
-  end
 
   depends_on "meson" => :test
   depends_on "ksysroot_native"
@@ -176,8 +170,8 @@ class KsysrootLoongarch64LinuxMusl < Formula
              "--cross-file=loongarch64-linux6.12-musl", testpath/"build"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build"
       # test for the executables
-      assert_predicate testpath/"build/test-c/main", :exist?
-      assert_predicate testpath/"build/test-cxx/main", :exist?
+      assert_path_exists testpath/"build/test-c/main"
+      assert_path_exists testpath/"build/test-cxx/main"
     end
   end
 end
