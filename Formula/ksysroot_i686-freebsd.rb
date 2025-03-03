050b0e28103993124fcf6f76b898758a7718b9c4
Keve <kevemueller@users.noreply.github.com>
2025-03-03 19:50:58 +0100

    update ksysroot_i686-freebsd

--- Formula/ksysroot_i686-freebsd.rb
+++ Formula/ksysroot_i686-freebsd.rb
@@ -4,14 +4,8 @@ class KsysrootI686Freebsd < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.1.tar.gz"
   sha256 "1091e2e6314c063e00234cb7694b5d03900c292f5025ebcc362770079a612d2e"
   license "BSD-2-Clause"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/kevemueller/ksysroot"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7fc5d52fbb73f3143566191880bcdf6cb03b45b90e1f3db8f2a8a6247817a166"
-    sha256 cellar: :any_skip_relocation, ventura:       "198f0354eaad01262d7dca502a41ebe9ce4df11b30ec66d8e2fcadd06f9248c2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f532ba78b433f9e867d713606e1c4f6ee9af26af850b781b9b954222f53230aa"
-  end
 
   depends_on "meson" => :test
   depends_on "ksysroot_native"
@@ -86,8 +80,8 @@ class KsysrootI686Freebsd < Formula
              "--cross-file=i686-freebsd14.2", testpath/"build"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build"
       # test for the executables
-      assert_predicate testpath/"build/test-c/main", :exist?
-      assert_predicate testpath/"build/test-cxx/main", :exist?
+      assert_path_exists testpath/"build/test-c/main"
+      assert_path_exists testpath/"build/test-cxx/main"
       # check pkg-config personality is properly set-up
       assert_equal "-lcrypto", shell_output("#{bin}/i686-freebsd14.2-pkg-config --libs libcrypto").strip
       assert_equal "", shell_output("#{bin}/i686-freebsd14.2-pkg-config --cflags libcrypto").strip
