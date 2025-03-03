ffa9e2c0908c5f956c88ff61bbe7ba309eb8c678
Keve <kevemueller@users.noreply.github.com>
2025-03-03 19:44:15 +0100

    update ksysroot_x86_64-netbsd10.0@10.0-netbsd

--- Formula/ksysroot_x86_64-netbsd10.0@10.0-netbsd.rb
+++ Formula/ksysroot_x86_64-netbsd10.0@10.0-netbsd.rb
@@ -4,6 +4,7 @@ class KsysrootX8664Netbsd100AT100Netbsd < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.1.tar.gz"
   sha256 "1091e2e6314c063e00234cb7694b5d03900c292f5025ebcc362770079a612d2e"
   license "BSD-4-Clause-UC"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
 
   depends_on "meson" => :test
@@ -88,8 +89,8 @@ class KsysrootX8664Netbsd100AT100Netbsd < Formula
              "--cross-file=x86_64-netbsd10.0", testpath/"build"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build"
       # test for the executables
-      assert_predicate testpath/"build/test-c/main", :exist?
-      assert_predicate testpath/"build/test-cxx/main", :exist?
+      assert_path_exists testpath/"build/test-c/main"
+      assert_path_exists testpath/"build/test-cxx/main"
     end
   end
 end
