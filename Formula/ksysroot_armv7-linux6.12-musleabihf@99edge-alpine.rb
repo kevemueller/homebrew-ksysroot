63426e8d51f7ffbae57d333b779705f5fea50668
Keve <kevemueller@users.noreply.github.com>
2025-03-03 19:54:26 +0100

    update ksysroot_armv7-linux6.12-musleabihf@99edge-alpine

--- Formula/ksysroot_armv7-linux6.12-musleabihf@99edge-alpine.rb
+++ Formula/ksysroot_armv7-linux6.12-musleabihf@99edge-alpine.rb
@@ -4,6 +4,7 @@ class KsysrootArmv7Linux612MusleabihfAT99edgeAlpine < Formula
   url "https://github.com/kevemueller/ksysroot/archive/refs/tags/v0.8.1.tar.gz"
   sha256 "1091e2e6314c063e00234cb7694b5d03900c292f5025ebcc362770079a612d2e"
   license "MIT"
+  revision 1
   head "https://github.com/kevemueller/ksysroot.git", using: :git, branch: "main"
 
   depends_on "meson" => :test
@@ -169,8 +170,8 @@ class KsysrootArmv7Linux612MusleabihfAT99edgeAlpine < Formula
              "--cross-file=armv7-linux6.12-musleabihf", testpath/"build"
       system Formula["meson"].bin/"meson", "compile", "-C", testpath/"build"
       # test for the executables
-      assert_predicate testpath/"build/test-c/main", :exist?
-      assert_predicate testpath/"build/test-cxx/main", :exist?
+      assert_path_exists testpath/"build/test-c/main"
+      assert_path_exists testpath/"build/test-cxx/main"
     end
   end
 end
